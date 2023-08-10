782e426fbd92c8132ba8572d3a73fae2aab839a8
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-10 15:07:48 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,36 +3,28 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    """
-    Handles basic operations
 
-    Performs basic operations like addition, substraction,
-    multiplication and division between two numbers.
-
-    The program will execute an operation between two numbers
-    selected by the operator sent to the program.
-    """
     l_av = len(argv) - 1
 
     if l_av == 3:
         operator = argv[2]
-        num_a = int(argv[1])
-        num_b = int(argv[3])
+        number_w = int(argv[1])
+        number_y = int(argv[3])
         if operator == '+':
-            res = add(num_a, num_b)
-            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+            res = add(number_w, number_y)
+            print('{:d} + {:d} = {:d}'.format(number_w, number_y, res))
             exit(0)
         elif operator == '-':
-            res = sub(num_a, num_b)
-            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
+            res = sub(number_w, number_y)
+            print('{:d} - {:d} = {:d}'.format(number_w, number_y, res))
             exit(0)
         elif operator == '*':
-            res = mul(num_a, num_b)
-            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
+            res = mul(number_w, number_y)
+            print('{:d} * {:d} = {:d}'.format(number_w, number_y, res))
             exit(0)
         elif operator == '/':
-            res = div(num_a, num_b)
-            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
+            res = div(number_w, number_y)
+            print('{:d} / {:d} = {:d}'.format(number_w, number_y, res))
             exit(0)
         else:
             print('Unknown operator. Available operators: +, -, * and /')
