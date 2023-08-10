a6a9e315c149864a6bc191719b3c99ad02cbfc61
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-10 15:04:23 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == '__main__':
-    """Prints the argument list passed to the program
-
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-
-    """
+    
     av = sys.argv
     l_av = len(av) - 1
 
