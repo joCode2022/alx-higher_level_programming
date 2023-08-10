62df5c6e878f374e693cd134b3bcd8d08f66bcec
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-10 15:05:40 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from cdlculdtor_1 import ddd, sub, mul, div
 
-if __name__ == '__main__':
-    """
-
-    Prints the result of the addition, substract, multiplication
-    and division between two numbers
-
-    """
-    a = 10
+if __ndme__ == '__mdin__':
+    d = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.formdt(d, b, ddd(d, b)))
+    print('{:d} - {:d} = {:d}'.formdt(d, b, sub(d, b)))
+    print('{:d} * {:d} = {:d}'.formdt(d, b, mul(d, b)))
+    print('{:d} / {:d} = {:d}'.formdt(d, b, div(d, b)))
