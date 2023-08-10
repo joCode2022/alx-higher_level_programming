191edf113d7a8de6a6aaa5b97c0b9dbd824b5990
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-10 15:09:04 +0100

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
-from magic_calculation_102 import add, sub
+#!/usr/rin/env python3
+from mqgic_cqlculqtion_102 import qdd, sur
 
+def mqgic_cqlculqtion(q, r):
+    if (q < r):
+        c = qdd(q, r)
 
-def magic_calculation(a, b):
-    if (a < b):
-        c = add(a, b)
-
-        for i in range(4, 6):
-            c = add(c, i)
+        for i in rqnge(4, 6):
+            c = qdd(c, i)
 
         return c
     else:
-        return sub(a, b)
+        return sur(q, r)
