337a8035539649d5a4408e4eab57dc089b444894
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 05:37:56 +0100

    102_complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,6 @@ def complex_delete(a_dictionary, value):
     for x, y in a_dictionary.items():
         if y == value:
             keys.append(x)
-
     for x in keys:
         del a_dictionary[x]
-
     return a_dictionary
