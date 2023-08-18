a452ab850c1db4f10ff0c0c7cf35c48a1884420e
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 05:38:50 +0100

    10_best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-
     b_score = max(a_dictionary.values(), default=None)
     for k, v in a_dictionary.items():
         if v == b_score:
