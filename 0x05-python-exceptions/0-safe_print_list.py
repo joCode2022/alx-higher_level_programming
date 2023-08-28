4396d74116f2559ed4b2db2533bc84a0b901712a
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-28 11:28:44 +0100

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(m_list=[], x=0):
     idx = 0
 
     try:
-        for i in my_list:
+        for i in m_list:
             if idx < x:
-                print('{}'.format(my_list[idx]), end='')
+                print('{}'.format(m_list[idx]), end='')
                 idx += 1
 
         print()
