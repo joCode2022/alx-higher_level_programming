bad55da5e511a30a5733cb3848a0df9df7645003
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-31 08:35:35 +0100

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
 This is the "4-print_square" module.
-The 4-print_square  module supplies one function, print_square(size).
+The 4-print_square  module supplies one function, print_square(square_size).
 """
 
-
-def print_square(size):
-    """prints a square with "#"'s that has a length of size """
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if size > 0:
-        print(("#" * size + "\n") * size, end="")
+def print_square(square_size):
+    """prints a square with "#"'s that has a length of square_size """
+    if type(square_size) is not int:
+        raise TypeError("square_size must be an integer")
+    if square_size < 0:
+        raise ValueError("square_size must be >= 0")
+    if square_size > 0:
+        print(("#" * square_size + "\n") * square_size, end="")
