f86641c6a76873165d556337f695ae2860e33c5b
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-31 08:29:55 +0100

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -4,14 +4,13 @@
  */
 
 #include "Python.h"
-
 /**
  * print_python_string - Prints information about Python strings.
  * @p: A PyObject string object.
  */
 void print_python_string(PyObject *p)
 {
-	long int length;
+	long int lgth;
 
 	fflush(stdout);
 
@@ -22,12 +21,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	length = ((PyASCIIObject *)(p))->length;
+	lgth = ((PyASCIIObject *)(p))->lgth;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	printf("  lgth: %ld\n", lgth);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &lgth));
 }
