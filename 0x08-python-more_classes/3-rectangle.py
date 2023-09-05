79ffdd79cb824d0d92aece71c20612d1e9b303fe
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-05 03:46:53 +0100

    0x08-python-more_classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,56 +4,56 @@
 
 class Rectangle:
     """The Rectangle class"""
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+    def __init__(self, w=0, h=0):
+        self.w = w
+        self.h = h
 
     @property
-    def width(self):
-        return self.__width
+    def w(self):
+        return self.__w
 
-    @width.setter
-    def width(self, value):
+    @w.setter
+    def w(self, value):
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("w must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("w must be >= 0")
 
-        self.__width = value
+        self.__w = value
 
     @property
-    def height(self):
-        return self.__height
+    def h(self):
+        return self.__h
 
-    @height.setter
-    def height(self, value):
+    @h.setter
+    def h(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("h must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("h must be >= 0")
 
-        self.__height = value
+        self.__h = value
 
     """Instance methods that return the area
         and perimeter of a rectangle"""
     def area(self):
-        return self.width * self.height
+        return self.w * self.h
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.w == 0 or self.h == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.w + self.h)
 
     """Prints out a rectangle in string form"""
     def __str__(self):
         rect = ""
 
-        if self.width == 0 or self.height == 0:
+        if self.w == 0 or self.h == 0:
             return rect
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.h):
+            for j in range(self.w):
                 rect += '#'
 
-            if i != self.height - 1:
+            if i != self.h - 1:
                 rect += '\n'
         return rect
