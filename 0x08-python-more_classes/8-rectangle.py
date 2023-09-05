b3eae4a028efa0db083db2ba2fa824582c7290f5
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-05 03:51:04 +0100

    0x08-python-more_classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -7,23 +7,23 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, w=0, height=0):
         Rectangle.number_of_instances += 1
-        self.width = width
+        self.w = w
         self.height = height
 
     @property
-    def width(self):
-        return self.__width
+    def w(self):
+        return self.__w
 
-    @width.setter
-    def width(self, value):
+    @w.setter
+    def w(self, value):
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("w must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("w must be >= 0")
 
-        self.__width = value
+        self.__w = value
 
     @property
     def height(self):
@@ -41,21 +41,21 @@ class Rectangle:
     """Instance methods that return the area
         and perimeter of a rectangle"""
     def area(self):
-        return self.width * self.height
+        return self.w * self.height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.w == 0 or self.height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.w + self.height)
 
     """Returns a rectangle in string form"""
     def __str__(self):
         rect = ""
 
-        if self.width == 0 or self.height == 0:
+        if self.w == 0 or self.height == 0:
             return rect
         for i in range(self.height):
-            for j in range(self.width):
+            for j in range(self.w):
                 rect += str(self.print_symbol)
 
             if i != self.height - 1:
@@ -64,7 +64,7 @@ class Rectangle:
 
     """Returns a string representation of the instance creation"""
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
+        return f"Rectangle({self.w}, {self.height})"
 
     """Runs when an instance is deleted"""
     def __del__(self):
