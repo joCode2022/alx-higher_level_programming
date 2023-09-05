39177bd729d9806ce4647e4d964168f804a22b8b
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-05 03:53:00 +0100

    0x08-python-more_classes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -4,9 +4,9 @@
 
 class Rectangle:
     """The Rectangle class"""
-    def __init__(self, width=0, height=0):
+    def __init__(self, width=0, h=0):
         self.width = width
-        self.height = height
+        self.h = h
 
     @property
     def width(self):
@@ -22,41 +22,41 @@ class Rectangle:
         self.__width = value
 
     @property
-    def height(self):
-        return self.__height
+    def h(self):
+        return self.__h
 
-    @height.setter
-    def height(self, value):
+    @h.setter
+    def h(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("h must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("h must be >= 0")
 
-        self.__height = value
+        self.__h = value
 
     """Instance methods that return the area
         and perimeter of a rectangle"""
     def area(self):
-        return self.width * self.height
+        return self.width * self.h
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.width == 0 or self.h == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.width + self.h)
 
     """Prints out a rectangle in string form"""
     def __str__(self):
         rect = ""
 
-        if self.width == 0 or self.height == 0:
+        if self.width == 0 or self.h == 0:
             return rect
-        for i in range(self.height):
+        for i in range(self.h):
             for j in range(self.width):
                 rect += '#'
 
-            if i != self.height - 1:
+            if i != self.h - 1:
                 rect += '\n'
         return rect
 
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
+        return f"Rectangle({self.width}, {self.h})"
