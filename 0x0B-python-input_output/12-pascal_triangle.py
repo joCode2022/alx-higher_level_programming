23200186d5285dba4f3fc03e2b5a5d4b474a5c18
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-13 05:37:35 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,24 +3,19 @@
 Returns a list of lists of integers
 representing the Pascal’s triangle of n.
 """
-
-
-def pascal_triangle(n):
+def pascal_triangle(n_o):
     """Returns the pascal triangle of n.
-
     Args:
         - n: size of the triangle (rows)
-
     Returns: a list of list of integers
     """
-
-    if n <= 0:
+    if n_o <= 0:
         return []
 
     l = [[0 for x in range(i + 1)] for i in range(n)]
     l[0] = [1]
 
-    for i in range(1, n):
+    for i in range(1, n_o):
         l[i][0] = 1
         for j in range(1, i + 1):
             if j < len(l[i - 1]):
