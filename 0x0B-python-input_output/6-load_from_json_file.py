2b32d8dae1a0346850868021b1e028c0cf407b57
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-13 05:40:49 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,10 +2,7 @@
 """Module 6-load_from_json_file.
 Creates an Object from a “JSON file”.
 """
-
-
 import json
-
 
 def load_from_json_file(filename):
     """Creates an object from filename.
@@ -13,6 +10,5 @@ def load_from_json_file(filename):
         - filename: name of the JSON file
     Returns: the object
     """
-
     with open(filename, 'r') as f:
         return json.load(f)
