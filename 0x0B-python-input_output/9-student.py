c55a019e1d0e03a073b636777ef8b6bbbbe42926
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-13 05:43:30 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,7 +3,6 @@
 Creates a Student class.
 """
 
-
 class Student:
     """Class that defines a student.
     Public attributes:
@@ -12,10 +11,8 @@ class Student:
         - age
     Public method to_json().
     """
-
     def __init__(self, first_name, last_name, age):
         """Initializes the Student instance."""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -25,5 +22,4 @@ class Student:
         of a Student instance.
         Returns: the dict representation of the instance.
         """
-
         return self.__dict__
