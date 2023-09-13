7046cfad94b26ac3e47b1121c34a66c801622d71
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-13 05:32:35 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,8 +3,6 @@
 Inserts a line of text to a file,
 after each line containing a specific string.
 """
-
-
 def append_after(filename="", search_string="", new_string=""):
     """Appends the new_string after
     the search_string in filename.
